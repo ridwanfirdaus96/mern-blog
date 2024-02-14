9281bc218dfb049c852e4f902e7884b04222d0fc
ridwanfirdaus96 <va.rhenka@gmail.com>
2024-02-14 21:22:00 +0700

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,31 @@
 export default function About() {
-    return (
-        <div>About</div>
-    )
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">
+            About Ridwan's Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Excepturi, unde quas accusamus optio quam quidem iste maxime esse
+              voluptatem, odio odit! Sunt facilis neque sequi saepe accusantium
+              architecto. Repellat, exercitationem.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quod
+              provident reprehenderit aperiam sunt iure numquam quia officia
+              cupiditate maxime?
+            </p>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui
+              voluptas ab perspiciatis ad tenetur eum libero nobis, impedit
+              aliquam eligendi!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
